77316e1f008cc7ff793aee6777902c6f06953b58
Vitaly Bikeev <shum41k@yandex.ru>
2025-07-28 08:38:38 +0400

    исправления

--- conftest.py
+++ conftest.py
@@ -1,18 +1,24 @@
 import pytest
 from main import BooksCollector
 
+
 @pytest.fixture
 def collection():
     return BooksCollector()
 
+
 def pytest_make_parametrize_id(val):
     return repr(val)
 
+
 @pytest.fixture
 def collection_five_books(collection):
-    books = ['Властелин колец', 'Король лев', 'Чужой', 'Сон в летнюю ночь', 'Молчание ягнят']
-    genres = ['Фантастика', 'Мультфильмы', 'Ужасы', 'Комедии', 'Детективы']
-    for book, genre in zip(books, genres):
-        collection.add_new_book(book)
-        collection.set_book_genre(book, genre)
+    
+    collection.books_genre = {
+        'Властелин колец': 'Фантастика',
+        'Король лев': 'Мультфильмы',
+        'Чужой': 'Ужасы',
+        'Сон в летнюю ночь': 'Комедии',
+        'Молчание ягнят': 'Детективы',
+    }
     return collection
